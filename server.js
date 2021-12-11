80aba4069f864cd4f76f8ac0b9909ebeb33f7a57
shridharGits <kalukheshridhar24@gmail.com>
2021-12-11 12:51:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs')
+let ignore = ['https://www.linkedin.com/shareArticle?mini=true&url=https://auth.geeksforgeeks.org/user/shindesahil61/practice',
+    'https://plus.google.com/share?url=https://auth.geeksforgeeks.org/user/shindesahil61/practice',
+    'https://twitter.com/share?url=https://auth.geeksforgeeks.org/user/shindesahil61/practice',
+    'https://www.facebook.com/sharer.php?u=https://auth.geeksforgeeks.org/user/shindesahil61/practice',
+    'https://auth.geeksforgeeks.org/?to=https://auth.geeksforgeeks.org/user/shindesahil61/practice',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/profile',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/practice/',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/articles',
+    'https://auth.geeksforgeeks.org/college/wce-sangli/',
+    'https://practice.geeksforgeeks.org/courses/fork-cpp',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/practice#problem-solved-div',
+    'https://practice.geeksforgeeks.org/questions/shindesahil61/',
+    'https://practice.geeksforgeeks.org/answers/shindesahil61/',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/practice#School',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/practice#Basic',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/practice#Easy',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/practice#Medium',
+    'https://auth.geeksforgeeks.org/user/shindesahil61/practice#Hard']
+
+let player1 = 'shindesahil61';
+let player2 = 'kalukheshridhar24';
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(`https://auth.geeksforgeeks.org/user/${player1}/practice`);
+    // await page.screenshot({path: 'geek.png'});
+    // await browser.waitForTarget(()=>false)
+
+    player1 = await page.$$eval('a', as => as.map(a => a.href));
+    await browser.close();
+
+})();
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(`https://auth.geeksforgeeks.org/user/${player2}/practice`);
+    await page.screenshot({ path: 'geek.png' });
+
+    player2 = await page.$$eval('a', as => as.map(a => a.href));
+    await browser.close();
+
+})();
+var delayInMilliseconds = 12000; // 20sec
+
+setTimeout(function () {
+    let difference = player1.filter(x => player2.indexOf(x) === -1);
+    difference = difference.filter(x => ignore.indexOf(x) == -1);
+    const saveNotes = function(difference){
+        const dataJSON = JSON.stringify(difference);
+        fs.writeFileSync('chalu kar bhosdike.json', dataJSON)
+    }
+    saveNotes(difference)
+}, delayInMilliseconds);
+
